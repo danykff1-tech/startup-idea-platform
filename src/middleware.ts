9d494ea01143247693156521e17eb3f9115cf739
danykff1-tech <danykff1@gmail.com>
2026-04-14 00:03:46 +0900

    Add files via upload

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const { pathname, searchParams, origin } = request.nextUrl
+
+  // 루트 URL에 ?code= 파라미터가 있으면 /auth/callback으로 전달
+  if (pathname === '/' && searchParams.has('code')) {
+    const code = searchParams.get('code')!
+    const url = new URL('/auth/callback', origin)
+    url.searchParams.set('code', code)
+    return NextResponse.redirect(url)
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: '/',
+}
